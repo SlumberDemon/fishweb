58253344e9ece29ba1b445ba13df02a538a1ea39
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-02-07 02:38:04 -0800

    rename test-apps to examples

--- examples/asgi/main.py
+++ examples/asgi/main.py
@@ -5,6 +5,6 @@ async def app(scope, receive, send):
             "type": "http.response.start",
             "status": 200,
             "headers": [[b"content-type", b"text/plain"]],
-        }
+        },
     )
     await send({"type": "http.response.body", "body": b"Hello from a pure ASGI app!"})
