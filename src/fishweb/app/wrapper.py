7a345d32d8fe5b23ae71631a4065f15793897056
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-02-10 00:24:11 -0800

    Fix exception handling during module imports

--- src/fishweb/app/wrapper.py
+++ src/fishweb/app/wrapper.py
@@ -13,6 +13,12 @@ if TYPE_CHECKING:
     from starlette.types import ASGIApp
 
 
+class AppStartupError(Exception):
+    def __init__(self, path: Path, *args: object) -> None:
+        super().__init__(*args)
+        self.path = path
+
+
 class AppWrapper:
     def __init__(self, app_dir: Path) -> None:
         self.app_dir = app_dir
@@ -35,11 +41,14 @@ class AppWrapper:
         try:
             logger.debug(f"executing module 'main' from {self.app_dir}")
             namespace = runpy.run_path(str(self.app_dir / "main.py"))
-            self.app = namespace["app"]
-        except ImportError:
-            logger.error(f"failed to execute module {self.app_dir / 'main'}")
-        except KeyError:
-            logger.error(f"app not found in module {self.app_dir / 'main'}")
+            try:
+                self.app = namespace["app"]
+            except KeyError:
+                logger.error(f"app not found in module {self.app_dir / 'main'}")
+        except Exception as exc:
+            msg = f"failed to execute module {self.app_dir / 'main'}"
+            logger.error(msg)
+            raise AppStartupError(self.app_dir / "main.py", msg) from exc
         finally:
             sys.path = original_sys_path
         return self.app
